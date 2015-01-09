dd4997f1ebf9cb400650d5ac3df1686b3fc6876f
Charles David <charles.david@zeuslearning.com>
2015-01-09 17:31:52 +0530

    Add code for request handlers

--- router.js
+++ router.js
@@ -1,5 +1,11 @@
-function route(pathname) {
+function route(handle,pathname) {
 	console.log("routing request to::"+pathname)
+	if (typeof handle[pathname] === 'function') {
+		handle[pathname]();
+	} else {
+		console.log('No such file found');
+	}
+	
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require("http"),
 	url = require("url");
 
-function start(route) {
+function start(route,handle) {
 http.createServer(function(request,response) {
 var pathname = url.parse(request.url).pathname;
 console.log("pathname is" + pathname);
 console.log('request received');
-route(pathname);
+route(handle,pathname);
 response.writeHead(200,{"Content-type":"text/plain"});
 response.write("hello world");
 response.end();
