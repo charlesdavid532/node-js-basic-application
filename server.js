92fe4b6c167102701d96b9c97ce48f799cc31d19
Charles David <charles.david@zeuslearning.com>
2015-01-09 15:42:04 +0530

    Create index and modularize code

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require("http");
 
+function start() {
 http.createServer(function(request,response) {
+console.log('request received');
 response.writeHead(200,{"Content-type":"text/plain"});
 response.write("hello world");
 response.end();
 }).listen(8887);
+
+console.log('Server has started');
+}
+
+exports.start = start;
