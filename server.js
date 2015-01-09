654b7d53d32b057092223bb06aec287d7b3944eb
Charles David <charles.david@zeuslearning.com>
2015-01-09 16:15:04 +0530

    Add router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var http = require("http");
+var http = require("http"),
+	url = require("url");
 
-function start() {
+function start(route) {
 http.createServer(function(request,response) {
+var pathname = url.parse(request.url).pathname;
+console.log("pathname is" + pathname);
 console.log('request received');
+route(pathname);
 response.writeHead(200,{"Content-type":"text/plain"});
 response.write("hello world");
 response.end();
