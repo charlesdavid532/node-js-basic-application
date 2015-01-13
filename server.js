467dc4397daf892a788515ee74ebd8e991291f31
Charles David <charles.david@zeuslearning.com>
2015-01-13 16:03:33 +0530

    Create database connection

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 var http = require("http"),
-	url = require("url");
+	url = require("url"),
+	mysql = require("mysql");
 
 function start(route,handle) {
-http.createServer(function(request,response) {
-	var pathname = url.parse(request.url).pathname,
-		postData = "";
-	console.log("pathname is" + pathname);
-	console.log('request received');
 
-	request.setEncoding("utf8");
-	request.addListener("data", function(postDataChunck) {
-		postData += postDataChunck;
-	});
-	request.addListener("end", function() {
-		route(handle,pathname,response,postData);
-	});
+var connection = mysql.createConnection({
+   user: "root",
+   password: "",
+  database: "test"
+});
+
+http.createServer(function (request, response) {
+
+    connection.query("INSERT INTO table1 (id,fname) values (8,'kbb');");
+	console.log('row inserted');
+    connection.query('SELECT * FROM table1;', function (error, rows, fields) {
+
+        response.writeHead(200, {
+            "Content-Type": "text/plain",
+            'Access-Control-Allow-Origin' : '*'
+        });
+        response.write(JSON.stringify(rows));
+        response.end();
+
+  });
+
 }).listen(8887);
 
 console.log('Server has started');
